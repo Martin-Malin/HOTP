ec27e9eef43234c3d7a7d4588cfb164be860adc9
Adrien Caillol <acaillol@sii.fr>
2023-05-02 16:27:12 +0200

    Gestion du paramètre counter en big endian La méthode DynamicTruncation retourne les octets dans le bon ordre

--- HOTP/HOTPComputation.cs
+++ HOTP/HOTPComputation.cs
@@ -7,19 +7,26 @@ namespace HOTP
     {
         public static int OtpLength = 6;
 
-        public static string ComputeHOTP(byte[] key, ulong counter)
+        public static string ComputeHOTP(byte[] key, long counter)
         {
-            //Compute HMAC SHA-1
-            byte[] byteCounter = BitConverter.GetBytes(counter);
+            byte[] byteCounter = GetBigEndianBytes(counter);
 
             byte[] hashedData = HMACSHA1.HashData(key, byteCounter);
 
             //Truncate
             byte[] truncatedData = DynamicTruncation(hashedData);
-            uint value = BitConverter.ToUInt32(truncatedData, 0);
+            int value = BitConverter.ToInt32(truncatedData, 0);
 
             //Return mod 10^Length
-            return (value % (Math.Pow(10, OtpLength))).ToString();
+            return (value % Math.Pow(10, OtpLength)).ToString();
+        }
+
+        private static byte[] GetBigEndianBytes(long counter)
+        {
+            byte[] byteCounter = BitConverter.GetBytes(counter);
+            Array.Reverse(byteCounter);
+
+            return byteCounter;
         }
 
         private static byte[] DynamicTruncation(byte[] hashedData)
@@ -31,10 +38,10 @@ namespace HOTP
             int offset = hashedData[19] & 0x0F;
 
             byte[] truncatedData = new byte[4];
-            for (int i = 0; i < truncatedData.Length; i++)
-            {
-                truncatedData[i] = hashedData[offset + i];
-            }
+            truncatedData[0] = hashedData[offset + 3]; //FF000000
+            truncatedData[1] = hashedData[offset + 2]; //00FF0000
+            truncatedData[2] = hashedData[offset + 1]; //0000FF00
+            truncatedData[3] = (byte)(hashedData[offset] & 0x7f); //000000FF - 0x7f Make the number positive
 
             return truncatedData;
         }
