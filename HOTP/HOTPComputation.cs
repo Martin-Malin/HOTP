3b922b3d0c7928e925c65fc0a8f3cf25f6a7e8b7
Adrien Caillol <acaillol@sii.fr>
2023-05-02 15:08:48 +0200

    Premier commit

---
+++ HOTP/HOTPComputation.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
+
+namespace HOTP
+{
+    internal class HOTPComputation
+    {
+        public static int OtpLength = 6;
+
+        public static string ComputeHOTP(byte[] key, ulong counter)
+        {
+            //Compute HMAC SHA-1
+            byte[] byteCounter = BitConverter.GetBytes(counter);
+
+            byte[] hashedData = HMACSHA1.HashData(key, byteCounter);
+
+            //Truncate
+            byte[] truncatedData = DynamicTruncation(hashedData);
+            uint value = BitConverter.ToUInt32(truncatedData, 0);
+
+            //Return mod 10^Length
+            return (value % (Math.Pow(10, OtpLength))).ToString();
+        }
+
+        private static byte[] DynamicTruncation(byte[] hashedData)
+        {
+            Contract.Requires(hashedData.Length >= 20);
+            Contract.Ensures(Contract.Result<byte[]>().Length == 4);
+
+            //Get 4 least significant bits of hashedData, as an int
+            int offset = hashedData[19] & 0x0F;
+
+            byte[] truncatedData = new byte[4];
+            for (int i = 0; i < truncatedData.Length; i++)
+            {
+                truncatedData[i] = hashedData[offset + i];
+            }
+
+            return truncatedData;
+        }
+    }
+}
---
+++ HOTP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using HOTP;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        byte[] key = Encoding.UTF8.GetBytes("8EjsRNNE5f4Kd2sELR3G1eLq9up5Kj2VofTnUfiyCUDUXaeWwvyc7WP9dlul");
+        ulong counter = 144L;
+
+        string hotpCode = HOTPComputation.ComputeHOTP(key, counter);
+
+        Console.WriteLine("DIY :");
+        Console.WriteLine(hotpCode);
+        Console.ReadLine();
+    }
+}
